1738d7c7d9f8909f586bfe5713df4296645ce2b5
xcheche <chekwubechukwu23@gmail.com>
2024-07-26 22:10:01 +0100

    added pagination

--- timetable/views.py
+++ timetable/views.py
@@ -3,45 +3,55 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from timetable.forms import TodoForm
 from timetable.models import Todo
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def index(request):
-    todos = Todo.objects.order_by('day').all()
-   
-    return render(request, 'timetable/index.html', {'todos': todos})
+    todos = Todo.objects.order_by("day").all()
+    page_number = request.GET.get(
+        "page", 1
+    )  # Default to page 1 if no page is specified
+
+    paginator = Paginator(todos, 2)
+    try:
+        todos = paginator.page(page_number)
+    except PageNotAnInteger:
+        todos = paginator.page(1)
+    except EmptyPage:
+        todos = paginator.page(paginator.num_pages)
+
+    return render(request, "timetable/index.html", {"todos": todos})
 
 
 def details(request, id):
     todos = Todo.objects.get(id=id)
-    return render(request, 'timetable/details.html', {'todos': todos})
-
+    return render(request, "timetable/details.html", {"todos": todos})
 
 
 def add(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect("index")
     else:
         form = TodoForm
 
-    return render(request, 'timetable/add.html', {'form': form})
-
+    return render(request, "timetable/add.html", {"form": form})
 
 
 def update(request, id):
     todos = Todo.objects.get(id=id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TodoForm(request.POST or None, instance=todos)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect("index")
     else:
         form = TodoForm(instance=todos)
 
-    return render(request, 'timetable/add.html', {'form': form, 'todos': todos})
+    return render(request, "timetable/add.html", {"form": form, "todos": todos})
 
 
 # step 1
@@ -52,33 +62,35 @@ def update(request, id):
 
 # step 2
 
+
 def delete(request, id):
     todos = Todo.objects.get(id=id)
-    if request.method == 'POST':
+    if request.method == "POST":
         todos.delete()
-        return redirect('index')
-    return render(request, 'timetable/delete.html', {'todos': todos})
+        return redirect("index")
+    return render(request, "timetable/delete.html", {"todos": todos})
 
 
-
-#search functionality
+# search functionality
 def search(request):
     if request.method == "POST":
-        search = request.POST.get('output')
+        search = request.POST.get("output")
         todos = Todo.objects.all()  # Initial queryset containing all records
-        
+
         if search:
-            todos = todos.filter(Q(title__icontains=search) |
-                                     Q(description__icontains=search) |
-                                     Q(favorite__icontains=search) |
-                                     Q(day__icontains=search) |
-                                     Q(month__icontains=search)|
-                                     Q(completed__icontains=search)|
-                                     Q(id__icontains=search))
-        
+            todos = todos.filter(
+                Q(title__icontains=search)
+                | Q(description__icontains=search)
+                | Q(favorite__icontains=search)
+                | Q(day__icontains=search)
+                | Q(month__icontains=search)
+                | Q(completed__icontains=search)
+                | Q(id__icontains=search)
+            )
+
         if not todos.exists():
             return HttpResponse("No results found matching the search criteria.")
-        
+
         return render(request, "timetable/index.html", {"todos": todos})
     else:
         return HttpResponse("An error occurred")
